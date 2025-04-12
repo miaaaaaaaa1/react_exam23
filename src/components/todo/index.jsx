caed16b2d702037eb8713b822e701da6eb9686c8
miaaaaaaaa1 <solomia1pochaievets@gmail.com>
2025-04-12 18:12:34 +0300

    index.jsx

--- src/components/todo/index.jsx
+++ src/components/todo/index.jsx
@@ -5,24 +5,22 @@ const todo = [
     {id: 2, name: 'Do my hw'},
     {id: 3, name: 'Iron my clothes'}
 ];
- console.log("ello")
  
 const Todo = ()=>{
     const dispatch = useDispatch();
  
     return(
         <div>
-            <h2>To do</h2>
-            <input type="text" className="inputadd" />
-             <ul>
-                {todo.map(to=>(
-                    <li key={todo.id}>
-                        {todo.name}
-                        <button onClick={()=>dispatch({type:'ADD_TO_LIST',payload: todo})}>Add to list</button>
-                     </li>
-                ))}
-            </ul>
-        </div>
+        <h2>To do</h2>
+        <ul>
+            {todo.map(todo=>(
+                <li key={todo.id}>
+                    {todo.name}
+                    <button onClick={()=>dispatch({type:'ADD_TO_LIST',payload: todo})}>Add to List</button>
+                </li>
+            ))}
+        </ul>
+    </div>
     )
 }
  
